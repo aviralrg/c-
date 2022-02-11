7e004e1278c809d98b54da0fe2562352f8b1041c
aviral_g <aviral.g@synconext.com>
2022-02-11 16:22:38 +0530

    destructor for the objects

--- main.cpp
+++ main.cpp
@@ -17,6 +17,10 @@ class employee
         id = e_id;
         name = e_name;
     }
+    ~employee()
+    {
+        cout << "destructor called for id " << id << endl;
+    }
     void print_id();
     void print_name()
     {
@@ -30,11 +34,16 @@ void employee::print_id()
 }
 int main (void)
 {
-    employee somebody;
-    somebody.print_name();
-    somebody.print_id();
     employee me("Avi",10);
     me.print_name();
     me.print_id();
+    for(int i = 0; i< 5; i++)
+    {
+        employee somebody;
+        somebody.print_name();
+        somebody.print_id();
+        somebody.id = i;
+        somebody.print_id();
+    }// destructor for these id in for loop will be called at the end of every iteration the scope of those objects end
     return 0;
-}
+}//destructor for the me obejct should be called last
