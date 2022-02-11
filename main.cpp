2448fbc0a6deb36213ad719a68bf2b2b9349181a
aviral_g <aviral.g@synconext.com>
2022-02-11 15:30:01 +0530

    basic class implemenation

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
-
+using namespace std;
+class human
+{
+    public:
+    string name;
+    void print_name()
+    {
+        cout << "His name is " << name;
+    }
+};
 int main (void)
 {
-    std::cout << "Hello World!";
+    human me;
+    me.name = "Avi";
+    me.print_name();
     return 0;
 }
